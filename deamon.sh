b3a0712a6ea574442dc242f5c33137c126db3213
Christian Laussmann <ch.laussmann@gmail.com>
2020-01-09 14:21:18 +0100

    REFACTOR: Always chop plot to same size

--- deamon.sh
+++ deamon.sh
@@ -2,8 +2,8 @@
 
 while true; do
 	# Chop file if it gets too long
-	if [[ $(wc -l ~/.battery_plot | awk '{print $1}') -ge 74 ]]; then
-		f=$(tail -n 60 ~/.battery_plot)
+	if [[ $(wc -l ~/.battery_plot | awk '{print $1}') -ge 75 ]]; then
+		f=$(tail -n 75 ~/.battery_plot)
 		echo "$f" > ~/.battery_plot
 	fi
 	
@@ -11,5 +11,5 @@ while true; do
 	upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | grep -Eo '[0-9]{1,3}' >> ~/.battery_plot
 	
 	# 5 min cycle
-	sleep 300
+	sleep 180
 done
