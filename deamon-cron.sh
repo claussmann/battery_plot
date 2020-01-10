554af28bfbd8dc50507bf62125b60a04925cea58
Christian Laussmann <ch.laussmann@gmail.com>
2020-01-10 16:31:06 +0100

    ADD: Script for integration in crontab

---
+++ deamon-cron.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Chop file if it gets too long
+if [[ $(wc -l ~/.batteryusage/battery_data | awk '{print $1}') -ge 500 ]]; then
+	f=$(tail -n 100 ~/.batteryusage/battery_data)
+	echo "$f" > ~/.batteryusage/battery_data
+fi
+	
+# Write current battery percentage to file
+upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | grep -Eo '[0-9]{1,3}' >> ~/.batteryusage/battery_data
